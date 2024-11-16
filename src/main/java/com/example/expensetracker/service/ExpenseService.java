b93be85b4e9c05b797a8244e1f4b4410473bde7c
suryanshjaryal <suryanshjaryal114@gmail.com>
2024-11-16 15:50:32 +0530

    new

---
+++ src/main/java/com/example/expensetracker/service/ExpenseService.java
@@ -0,0 +1,34 @@
+package com.example.expensetracker.service;
+
+import com.example.expensetracker.model.Expense;
+import com.example.expensetracker.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
+@Service
+public class ExpenseService {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public void saveExpense(Expense expense) {
+        expenseRepository.save(expense);
+    }
+
+    public Expense getExpenseById(Long id) {
+        return expenseRepository.findById(id).orElse(null);
+    }
+
+    public void deleteExpenseById(Long id) {
+        expenseRepository.deleteById(id);
+    }
+}
+
